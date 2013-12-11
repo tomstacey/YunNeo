c130011ca66bb6a9f2bccb7f02c1763dc0e0f897
tomstacey <tom@Toms-MacBook-Air-11.local>
2013-12-11 15:52:05 +0000

    Working strandlighter
    Works when fed a value over serial.
    Has beta version of flashing code that would flash a grade value over
    the 'percentage' value.
    To Do:
    +Turn percents into a value that works on the number of LEDs on the
    strip
    + Pull in grade value from serial

--- serial_strandlighter_2.ino
+++ serial_strandlighter_2.ino
@@ -7,13 +7,19 @@
 //Create strip1 and strip2
 Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(8, PIN8, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(8, PIN9, NEO_GRB + NEO_KHZ800);
+
+// written my Mr Shaw !!!!!
  
 int inputPercent = 0;
- int changed = 0;
+int changed = 0;
+int grade = 2;
+uint16_t global_delay_count;
 
-void setup() {
+
+void setup() 
+{
    // initialize serial:
-  Serial.begin(9600);
+  Serial.begin(19200);
    // Setup Strips
   strip1.begin();
 
@@ -21,72 +27,113 @@ void setup() {
   strip2.setBrightness(50);
 }
 
-void loop() {
- strip1.show();
- getPercentFromSerial();
- halfAndShow();
+void loop() 
+{
+  strip1.show();
+  getPercentFromSerial();
+  flashGrade();
 
- 
- //Serial.print(inputPercent);
 }
 
- int getPercentFromSerial() {
-  
-    //serial stuff
+int getPercentFromSerial() 
+{
+  //serial stuff
   if ( Serial.available()) // Check to see if at least one character is available
   {
     char ch = Serial.read();
     if( isDigit(ch) ) // is this an ascii digit between 0 and 9?
     {
        inputPercent = (ch - '0');      // ASCII value converted to numeric value 
-     }
     }
-    return inputPercent;
-  }
-
-
-void halfAndShow() {
- 
- 
-      if (inputPercent > 0 && inputPercent >= changed){
-      
-      for (int i = 0; i < inputPercent; i++){
-      // for (int j = 0; j <= inputPercent; j++) {
-         strip1.setPixelColor(i, 255, 0, 255);
-         strip1.show();       
-     }
-     // }
-       changed = inputPercent;
-      
-      }
-      
-    else if (inputPercent < changed){
-      
-      int m = strip1.numPixels() - inputPercent;
-        for (int n = 0; n < m; n++){
-         strip1.setPixelColor(n +1, 0, 0, 0);
-         strip1.show();
-        }
-      
-    for (int k = 0; k < inputPercent; k++){
-   // for (int l = 0; l <= inputPercent; l++) {
-         
-        strip1.setPixelColor(k, 255,0 , 255);
-        strip1.show();
-        //Serial.print(k);
-        
-
    }
-   // }
-    }
-    
-    
-
-    }
+    return inputPercent;
+ }
 
 
+void showAmount()
+{
+int i;  
+  if(inputPercent == 0)
+  {
+      for (i = 0; i <=7; i++)
+      {
+        strip1.setPixelColor(i, 255, 255, 255);
+      }    
+  }
+  else if(inputPercent == 8)
+  {
 
-       
+      for (i = 0; i <=7; i++)
+      {
+        strip1.setPixelColor(i, 255, 0, 255);
+      }    
+  }
+  else if (inputPercent >= 0 && inputPercent <= 8)
+ {
+  
+      for (i = 0; i <=inputPercent-1; i++)
+      {
+        strip1.setPixelColor(i, 255, 0, 255);
+      }  
+      for (i = inputPercent; i <= 7; i++)
+      {
+        strip1.setPixelColor(i, 255, 255, 255);
+      }  
+
+ } 
+ 
+     strip1.show();         
+}
+
+void flashGrade()
+{
+  uint16_t i;
+  delay(1);
+  global_delay_count++;
+  if(global_delay_count==1000)
+  {      
+       for (i = 0; i <=5; i++)
+      {
+        strip1.setPixelColor(i, 0, 0, 0);
+      }    
+     
+  }
+ if(global_delay_count==1500)
+{
+  
+    showAmount(); 
+    global_delay_count=0;  
+}
+
+     strip1.show();     
+
+}
+
+
+
+
+/*
+gradeflash()
+{
+ 
+ if(gradeinput == 2) 
+ {
+    strip1.show();
+   delay(100);
+    strip1.showall off 
+
+   
+ }
+  
+  
+  
+}
+
+
+*/
+
+
+   
        
 
     
